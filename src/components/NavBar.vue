a899f0c35ab8563b300bd2570bc20b3f0cb28f47
MarioGuTe <mariogutierreztello@gmail.com>
2024-09-28 21:53:39 -0300

    add navbar basic structure

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,3 +1,26 @@
 <template>
-  <h3>This is the NavBar</h3>
+  <nav>
+    <ul>
+      <a href=""><li>Curriculum</li></a>
+    </ul>
+  </nav>
 </template>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: flex-end;
+  padding: 1rem;
+  width: 100%;
+}
+
+ul {
+  border: 2px solid red;
+}
+
+li {
+  background-color: blueviolet;
+  padding: 0.75rem 2rem;
+  color: white;
+}
+</style>
